7ed6aafecb374e01f7e17f0898b28aa17f51402e
Jin Cheng <jincheng411@gmail.com>
2020-12-31 19:21:32 -0500

    finish styling

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Dice from './Dice';
+import './RollDice.css'
 class RollDice extends Component {
   static defaultProps = {
     sides: ["one", "two", "three", "four", "five", "six"]
   }
   constructor(props) {
     super(props);
-    this.state = { die1: 'one', die2: 'one'}
+    this.state = { die1: 'one', die2: 'one' }
     this.roll = this.roll.bind(this);
   }
   roll() {
@@ -16,13 +17,15 @@ class RollDice extends Component {
     const newDice2 = this.props.sides[
       Math.floor(Math.random() * this.props.sides.length)
     ];
-    this.setState({ die1: newDice1, die2: newDice2});
+    this.setState({ die1: newDice1, die2: newDice2 });
   }
   render() {
     return (
       <div className="RollDice">
-        <Dice face={this.state.die1} />
-        <Dice face={this.state.die2} />
+        <div className="RollDice-container">
+          <Dice face={this.state.die1} />
+          <Dice face={this.state.die2} />
+        </div>
         <button onClick={this.roll}>Roll Dice!!</button>
       </div>
     )
